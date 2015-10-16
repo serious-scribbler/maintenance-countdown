e8a63ab4ea4a358166627c8ad71b50962cb3e9d7
serious-scribbler <contact@pniehus.de>
2015-10-16 22:25:29 +0200

    Added the plugin php file

---
+++ maintenance-countdown.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Maintenance Countdown
+ * Plugin URI: 
+ * Description: This plugin adds a maintenance countdown page to your website giving you the opportunity to let your visitors know when your site will be online again.
+ * Version: 1.0.0
+ * Author: Phil Niehus
+ * Author URI: http://pniehus.de
+ * License: GPL2
+ */
+ ?>
